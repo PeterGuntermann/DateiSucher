f3fed921ab1176002075c3fca309a16fc01753ff
Peter Guntermann <pgu@posteo.de>
2020-10-25 14:47:18 +0100

    Create multiple pages

---
+++ DateiSucher/WpfPlayground.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DateiSucher
+{
+    /// <summary>
+    /// Interaction logic for WpfPlayground.xaml
+    /// </summary>
+    public partial class WpfPlayground : Page
+    {
+        public WpfPlayground()
+        {
+            InitializeComponent();
+        }
+    }
+}
