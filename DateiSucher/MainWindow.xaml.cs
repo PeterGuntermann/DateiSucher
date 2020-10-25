a1f4f766117cc636f868ce35ba7517dddcdc0310
Peter Guntermann <pgu@posteo.de>
2020-10-25 13:49:06 +0100

    Fill GUI in C# experimentally

--- DateiSucher/MainWindow.xaml.cs
+++ DateiSucher/MainWindow.xaml.cs
@@ -23,6 +23,32 @@ namespace DateiSucher
         public MainWindow()
         {
             InitializeComponent();
+
+            Grid grid = new Grid();
+            this.Content = grid;
+            Button btn = new Button();
+            btn.FontSize = 26;
+            btn.Width = 250;
+            btn.Height = 100;
+
+            WrapPanel wrapPanel = new WrapPanel();
+            TextBlock txt = new TextBlock();
+            txt.Text = "Multi";
+            txt.Foreground = Brushes.Blue;
+            wrapPanel.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "Color";
+            txt.Foreground = Brushes.Red;
+            wrapPanel.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "Button";
+            txt.Foreground = Brushes.Green;
+            wrapPanel.Children.Add(txt);
+
+            btn.Content = wrapPanel;
+            grid.Children.Add(btn);
         }
     }
 }
