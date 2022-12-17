d8e03c55a5f4026939d96f5b9a9686a3330035e1
Peter Guntermann <pgu@posteo.de>
2022-12-17 22:04:12 +0100

    Extract UserControl

--- DateiSucher/MainWindow.xaml.cs
+++ DateiSucher/MainWindow.xaml.cs
@@ -3,34 +3,33 @@ using AdonisUI.Controls;
 using MessageBox = System.Windows.MessageBox;
 using WinForms = System.Windows.Forms;
 
-namespace DateiSucher
+namespace DateiSucher;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void SearchButton_Click(object sender, RoutedEventArgs e)
-        {
-            // TODO: Not yet implemented.
-            MessageBox.Show("Not yet implemented.");
-        }
+    private void SearchButton_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO: Not yet implemented.
+        MessageBox.Show("Not yet implemented.");
+    }
 
-        private void ChooseFolderButton_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new WinForms.FolderBrowserDialog();
-            if (SearchedFolder.Text != string.Empty)
-                dialog.SelectedPath = SearchedFolder.Text;
+    private void ChooseFolderButton_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new WinForms.FolderBrowserDialog();
+        if (SearchedFolder.Text != string.Empty)
+            dialog.SelectedPath = SearchedFolder.Text;
 
-            var dialogResult = dialog.ShowDialog();
+        var dialogResult = dialog.ShowDialog();
 
-            if (dialogResult == WinForms.DialogResult.OK)
-                SearchedFolder.Text = dialog.SelectedPath;
-        }
+        if (dialogResult == WinForms.DialogResult.OK)
+            SearchedFolder.Text = dialog.SelectedPath;
     }
 }
---
+++ DateiSucher/UserControls/FileTree.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace DateiSucher.UserControls;
+
+public partial class FileTree : UserControl
+{
+    public FileTree()
+    {
+        InitializeComponent();
+    }
+}
