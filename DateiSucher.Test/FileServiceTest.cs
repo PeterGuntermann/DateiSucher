ff609ada51d2285dc4a32300344f1248594fcda6
Peter Guntermann <pgu@posteo.de>
2022-12-18 19:55:39 +0100

    Test file detection

--- DateiSucher.Test/FileServiceTest.cs
+++ DateiSucher.Test/FileServiceTest.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -7,28 +7,46 @@ namespace DateiSucher.Test;
 
 public class FileServiceTest
 {
-    static class TestFiles
+    static class Test
     {
-        public static string FileInRoot => nameof(FileInRoot);
-        public static string CopyOfFileInRoot => nameof(CopyOfFileInRoot);
-        public static string FileA => nameof(FileA);
-        public static string FileB => nameof(FileB);
-        public static string FileC => nameof(FileC);
+        public static class File
+        {
+            public static string FileInRoot => nameof(FileInRoot) + ".txt";
+            public static string CopyOfFileInRoot => nameof(CopyOfFileInRoot) + ".txt";
+            public static string FileA => nameof(FileA) + ".txt";
+            public static string FileB => nameof(FileB) + ".txt";
+            public static string FileC => nameof(FileC) + ".txt";
+        }
+
+        public static class Folder
+        {
+            public static string Root => @"..\..\..\TestFileStructure";
+            public static string SubfolderA => nameof(SubfolderA);
+            public static string SubfolderB => nameof(SubfolderB);
+            public static string SubfolderC => nameof(SubfolderC);
+        }
     }
-    
+
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void ShouldCountFilesAndSubfolders()
+    public void ShouldDetectFilesAndSubfolders()
     {
-        var numFiles = 2;
-        IEnumerable<string> fileNames = new List<string> { "FileInRoot.txt", "CopyOfFileInRoot.txt" };
-        
-        numFiles.Should().Be(2);
-        
+        var subfolders = Directory.GetDirectories(Test.Folder.Root);
+        var subfolderInfos = subfolders.Select(folder => new DirectoryInfo(folder)).ToList();
+        var files = Directory.GetFiles(Test.Folder.Root);
+        var fileInfos = files.Select(file => new FileInfo(file)).ToList();
+
+        files.Length.Should().Be(2);
+        fileInfos[0].Name.Should().Be(Test.File.CopyOfFileInRoot);
+        fileInfos[1].Name.Should().Be(Test.File.FileInRoot);
+
+        subfolders.Length.Should().Be(2);
+        subfolderInfos[0].Name.Should().Be(Test.Folder.SubfolderA);
+        subfolderInfos[1].Name.Should().Be(Test.Folder.SubfolderB);
     }
 
     [Test]
