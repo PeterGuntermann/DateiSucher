8b82191f6dfaf1102487cec7232142bddb3758eb
Peter Guntermann <pgu@posteo.de>
2022-12-18 20:07:09 +0100

    Folder model

---
+++ DateiSucher/Folder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DateiSucher;
+
+public class Folder
+{
+    public Folder(string path)
+    {
+        Path = path;
+    }
+
+    public string Path { get; }
+
+    public IEnumerable<Folder> Subfolders => Directory
+        .GetDirectories(Path)
+        .Select(dir => new Folder(dir));
+    
+    public DirectoryInfo DirectoryInfo => new DirectoryInfo(Path);
+
+    public IEnumerable<FileInfo> Files => Directory
+        .GetFiles(Path)
+        .Select(file => new FileInfo(file));
+}
